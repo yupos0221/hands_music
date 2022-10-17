8ce741e6ea064755c32a464be64e4269fbf36d4f
katorapa21 <katorapa21@gmail.com>
2022-10-17 22:51:08 +0900

    update single mode

--- sketch.js
+++ sketch.js
@@ -12,6 +12,8 @@ let buttonTrack;
 
 let sliderVelocity;
 
+let radioMode;
+
 let onHandTracking = false;
 let onView = false;
 
@@ -34,6 +36,7 @@ const note_colors = [{8:["C", "#4040ff"], 12:["D", "#ffa040"], 16:["E", "#ff00ff
 const leftTones = [["C3", "D3", "E3", "F3"], ["C4", "D4", "E4", "F4"], ["C5", "D5", "E5", "F5"]];
 const rightTones = [["G3", "A3", "B3", "C4"], ["G4", "A4", "B4", "C5"], ["G5", "A5", "B5", "C6"]];
 let toneShift = [1,1];
+const singleTones = [["G3", "A3", "B3", "C4"], ["C4", "D4", "E4", "F4"], ["G4", "A4", "B4", "C5"]];
 // let times = [Tone.now(),Tone.now(),Tone.now(),Tone.now()];
 let leftTimes = [Tone.now(),Tone.now(),Tone.now(),Tone.now()];
 let rightTimes = [Tone.now(),Tone.now(),Tone.now(),Tone.now()];
@@ -113,8 +116,14 @@ function setup() {
   buttonTrack.style("font-size: x-large");
 
   sliderVelocity = createSlider(0, 1, 0.5, 0.01);
-  sliderVelocity.position(sizeButton[0]*3, sizeButton[1]/2-15);
+  sliderVelocity.position(sizeButton[0]*3+30, sizeButton[1]/2-15);
   
+  radioMode = createRadio();
+  radioMode.position(sizeButton[0]*3+30, sizeButton[1]/2+15);
+  radioMode.option("Double hand");
+  radioMode.option("Single hand");
+  radioMode.selected("Double hand");
+
   // 映像を非表示化
   capture.hide();
 }
@@ -136,8 +145,14 @@ function draw() {
   // 手の頂点を表示
   
   if(onHandTracking){
-    drawHands();
-    playHandsSound();
+    if(radioMode.value() == "Double hand"){
+      drawDoubleHands();
+      playDoubleHandsSound();
+    }else if(radioMode.value() == "Single hand"){
+      drawSingleHands();
+      playSingleHandsSound();
+    }
+    
   }
 
   updateView();
@@ -157,7 +172,8 @@ function updateView(){
     buttonView.position(sizeButton[0], 5);
     buttonTrack.size(sizeButton[0], sizeButton[1]-20);
     buttonTrack.position(sizeButton[0]*2, 5);
-    sliderVelocity.position(sizeButton[0]*3, sizeButton[1]/2-15);
+    sliderVelocity.position(sizeButton[0]*3+30, sizeButton[1]/2-15);
+    radioMode.position(sizeButton[0]*3+30, sizeButton[1]/2+15);
   }
   pWindowWidth = windowWidth;
 }
@@ -180,7 +196,7 @@ function startHandTracking(){
   onHandTracking = !onHandTracking;
 }
 
-function drawHands() {
+function drawDoubleHands() {
   
   const hands = handsfree.data?.hands;
 
@@ -259,7 +275,7 @@ function drawHands() {
   
 }
 
-function playHandsSound(){
+function playDoubleHandsSound(){
   velocity = sliderVelocity.value();
 
   const hands = handsfree.data?.hands;
@@ -296,7 +312,96 @@ function playHandsSound(){
   console.log(leftTrigger);
   console.log(rightTrigger);
 
+} 
+
+function drawSingleHands() {
+  
+  const hands = handsfree.data?.hands;
+  if (!hands?.multiHandLandmarks) return;
+
+  const zip = (array1, array2) => array1.map((_, i) => [array1[i], array2[i]]);
+  zip(hands.multiHandLandmarks, hands.multiHandedness).forEach(([hand, handedness]) => {
+
+    label = handedness["label"] == 'Left' ? 1 : 0;
+    hand.forEach((landmark, landmarkIndex) => {
+      push();
+      switch (landmarkIndex) {
+        case 4:
+          fill("#9b5de5");
+          circle(width - landmark.x * width, landmark.y * height, circleSize*2);
+          break;
+        case 8:
+        case 12:
+        case 16:
+        case 20:
+          fill(note_colors[(toneShift[label]+1)%2][landmarkIndex][1]);
+          circle(width - landmark.x * width, landmark.y * height, circleSize*2);
+          fill("#000000");
+          textSize(16);
+          textStyle(BOLD);
+          text(note_colors[(toneShift[label]+1)%2][landmarkIndex][0], width - landmark.x * width-textWidth(note_colors[label][landmarkIndex][0])/2, landmark.y * height+textWidth(note_colors[label][landmarkIndex][0])/2);
+          break;
+        case 21:
+          let y = landmark.y*height;
+          print(landmark.y, y, height/3);
+          if(y < height/3){
+            toneShift[label] = 2;
+          }else if(y >= height/3 && y < height*2/3){
+            toneShift[label] = 1;
+          }else{
+            toneShift[label] = 0;
+          }
+          fill(color(255, 255, 255));
+          circle(width - landmark.x * width, landmark.y * height, circleSize);
+          break;
+        default:
+          fill(color(255, 255, 255));
+          circle(width - landmark.x * width, landmark.y * height, circleSize);
+      }
+      pop();
+    });
+  });
+
+  
 }
+
+function playSingleHandsSound(){
+  velocity = sliderVelocity.value();
+
+  const hands = handsfree.data?.hands;
+  if (!hands?.multiHandLandmarks) return;
+
+  // index, middle, ring, pinky
+  const leftHandPinch  = hands.pinchState[0]
+  const rightHandPinch = hands.pinchState[1]
+
+  leftHandPinch.forEach((handPinch, index) => {
+    if (handPinch == "held" && !leftTrigger[index]){
+      leftTimes[index] = Tone.now();
+      leftSynth[index].triggerAttack(singleTones[toneShift[0]][index], leftTimes[index], velocity);
+      leftTrigger[index] = true;
+    }else if(handPinch == "released" && leftTrigger[index]){
+      // console.log("stop")
+      leftSynth[index].triggerRelease()
+      leftTrigger[index] = false;
+    }
+  });
+
+  rightHandPinch.forEach((handPinch, index) => {
+    if (handPinch == "held" && !rightTrigger[index]){
+      rightTimes[index] = Tone.now();
+      rightSynth[index].triggerAttack(singleTones[toneShift[1]][index], rightTimes[index], velocity);
+      rightTrigger[index] = true;
+    }else if(handPinch == "released" && rightTrigger[index]){
+      // console.log("stop")
+      rightSynth[index].triggerRelease()
+      rightTrigger[index] = false;
+    }
+  });
+  console.log(leftTrigger);
+  console.log(rightTrigger);
+
+} 
 
 // function mouseClicked(){
 //   if(started){
